ec56c4f080f19d53beb1d9b886507496bce3c03c
LeonardoAgnelo <leonardoagnelo2001@gmail.com>
2022-03-31 16:59:38 -0300

    cadastrar agencia javascript

---
+++ src/js/cadastro-agencia.js
@@ -0,0 +1,21 @@
+var xmlhttp = new XMLHttpRequest();
+var form = document.getElementById("form-cadastro-agencia");
+
+form.addEventListener("submit", async function (event){
+    event.preventDefault();
+
+    var url = "http://localhost:8080/agencias";
+    var formData = new FormData(form);
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(Object.fromEntries(formData)),
+    })
+
+    //console.log(response.json())
+
+})
