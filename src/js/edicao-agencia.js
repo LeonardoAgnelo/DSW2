ff157884d6eeff875fae4556c0b299d25db2a7bc
LeonardoAgnelo <leonardoagnelo2001@gmail.com>
2022-03-31 21:08:33 -0300

    edição agencia js

--- src/js/edicao-agencia.js
+++ src/js/edicao-agencia.js
@@ -5,15 +5,15 @@ var form = document.getElementById("form-cadastro-agencia");
 
 form.addEventListener("submit", async function (event) {
   event.preventDefault();
-  var urltrue = window.location.pathname;
-  var urlAux = urltrue.split('/');
-  var cardID = urlAux[2]
-  var url = "http://localhost:3333/pacotes/agencia/"+cardID; // loca:3333/pacoste/agencia/+id
-  console.log(cardID);
+  const querystring =  window.location.search
+  const urlParam = new URLSearchParams(querystring);
+  const id =  urlParam.get('id')
+  var url = "http://localhost:3333/pacotes/agencias/" + id; // loca:3333/pacoste/agencia/+id
+  console.log(id);
   var formData = new FormData(form);
 
   const response = await fetch(url, {
-    method: 'POST',
+    method: 'PUT',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
